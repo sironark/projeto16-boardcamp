d8dd7a62c41de4bae9d12cbca5eb22cfce50b296
Gabriel Oliveira <gabriel_lopeslomeu@hotmail.com>
2023-09-24 11:32:26 -0300

    feat: config database production4

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -9,5 +9,6 @@ const configDatabase = {
     connectionString: process.env.DATABASE_URL,
   };
   if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
+  
 const db = new Pool(configDatabase);
 export default db 
